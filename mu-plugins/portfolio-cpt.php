cc7384a23db45ea011aa0b25ac1d4e29ab459402
toxzwaza <toxzprogram@gmail.com>
2026-04-15 19:21:46 +0900

    初期コミット: VPSの wp-content スナップショット (2026-04-15)

---
+++ mu-plugins/portfolio-cpt.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Onsite Solution - Portfolio CPT
+ * Description: ポートフォリオ用カスタム投稿タイプ
+ */
+add_action('init', function () {
+    register_post_type('portfolio', [
+        'label' => 'ポートフォリオ',
+        'labels' => [
+            'name' => 'ポートフォリオ',
+            'singular_name' => 'ポートフォリオ',
+            'add_new' => '新規追加',
+            'add_new_item' => '新しい実績を追加',
+            'edit_item' => '実績を編集',
+            'all_items' => 'すべての実績',
+            'menu_name' => 'ポートフォリオ',
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-portfolio',
+        'supports' => ['title', 'editor', 'thumbnail', 'excerpt'],
+        'show_in_rest' => true,
+        'rewrite' => ['slug' => 'portfolio'],
+        'taxonomies' => ['portfolio_category'],
+    ]);
+
+    register_taxonomy('portfolio_category', 'portfolio', [
+        'label' => 'カテゴリー',
+        'hierarchical' => true,
+        'show_in_rest' => true,
+        'rewrite' => ['slug' => 'portfolio-category'],
+    ]);
+});
